9960ae9030fcb9f157332428c10f42dd91cf4493
Alexey Kadantsev <fruktsky@gmail.com>
2023-03-09 17:33:42 +0300

    add alias

--- src/pages/AuthPage/AuthPage.tsx
+++ src/pages/AuthPage/AuthPage.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
-import AuthForm from "../../components/AuthForm";
+import AuthForm from "@components/AuthForm";
 import { CustomizedContainer } from "./styles";
-import logo from "../../assets/image/movie-logo.webp";
+import logo from "@images/movie-logo.webp";
 
 const AuthPage = () => {
   useEffect(() => {
