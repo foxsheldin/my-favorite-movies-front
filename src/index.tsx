1359bc7820259f7ffd588123cd99ed3c792545a3
Alexey Kadantsev <fruktsky@gmail.com>
2023-03-09 12:16:23 +0300

    add AuthPage

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import CssBaseline from "@mui/material/CssBaseline";
 import App from "./App";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    background: {
+      default: "#edeef1",
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
