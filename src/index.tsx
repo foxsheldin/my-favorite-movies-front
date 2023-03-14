0170ac215bf8b59799189e1aced75bf4e8411c3e
Alexey Kadantsev <fruktsky@gmail.com>
2023-03-14 16:06:45 +0300

    install axios, reduxjs/toolkit

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,19 @@ import CssBaseline from "@mui/material/CssBaseline";
 import App from "./App";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./theme";
+import { Provider } from "react-redux";
+import store from "./store";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
