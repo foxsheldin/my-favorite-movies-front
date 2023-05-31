8260c0d86d5d52c01cab75cf16ca299d5ecc85c5
Alexey Kadantsev <alexey.kadantsev@kodep.ru>
2023-05-31 15:26:59 +0300

    some fix

--- src/api/graphql/queries/getProfile.ts
+++ src/api/graphql/queries/getProfile.ts
@@ -3,6 +3,7 @@ import { gql } from "@apollo/client";
 export const GET_PROFILE = gql`
   query getProfile($userId: String!) {
     getProfile(userId: $userId) {
+      id
       email
       favoriteGenres {
         genreId
