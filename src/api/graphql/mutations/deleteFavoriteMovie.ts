f0359bb9a7af1c3d82efb798b3df9b2fd0929831
Alexey Kadantsev <alexey.kadantsev@kodep.ru>
2023-05-31 17:21:17 +0300

    fix: use fragments

--- src/api/graphql/mutations/deleteFavoriteMovie.ts
+++ src/api/graphql/mutations/deleteFavoriteMovie.ts
@@ -3,12 +3,7 @@ import { gql } from "@apollo/client";
 export const DELETE_FAVORITE_MOVIE = gql`
   mutation deleteFavoriteMovie($userId: String!, $movieId: Number!) {
     deleteFavoriteMovie(userId: $userId, movieId: $movieId) {
-      id
-      title
-      posterPath
-      overview
-      isFavorite
-      isWatched
+      ...CommonMovieFields
     }
   }
 `;
